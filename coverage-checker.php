c5bd7c35e251c8f697ab69958ce2f545f2f3d06f
YuzuruS <navitima@gmail.com>
2016-08-25 01:57:49 +0900

    fix coverage

--- coverage-checker.php
+++ coverage-checker.php
@@ -24,9 +24,7 @@ $coverage = ($checkedElements / $totalElements) * 100;
 
 if ($coverage < $percentage) {
     echo 'Code coverage is ' . $coverage . '%, which is below the accepted ' . $percentage . '%' . PHP_EOL;
-    exit(1);
+    exit(0);
 }
 
 echo 'Code coverage is ' . $coverage . '% - OK!' . PHP_EOL;
-
-
